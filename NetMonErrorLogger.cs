9ac7759a7426b7f1803d0324883b5c2284879757
Teethedev@outlook.com <Teethedev@outlook.com>
2022-01-05 00:24:00 +0200

    Add location of log file location.

--- NetMonErrorLogger.cs
+++ NetMonErrorLogger.cs
@@ -11,6 +11,10 @@ namespace NetworkMonitor
                 Directory.CreateDirectory(path);
 
             File.WriteAllText($@"{path}\{filename}.log", $"Message: {ex.Message}\n\nStack: {ex.StackTrace}\n");
+
+            Console.WriteLine($"\n\n\tError log can be found at '{path}'.");
+
+            System.Threading.Thread.Sleep(5000);
         }
     }
 }
